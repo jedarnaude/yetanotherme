9dce436ce097a800df8d4f940f4099c08f6de7bf
Jose Enrique D'Arnaude <jedarnaude@prezi.com>
2013-05-13 02:00:16 +0200

    changed position of elements and look and feel of md list

--- config.rb
+++ config.rb
@@ -8,13 +8,13 @@ images_dir = "web/img"
 javascripts_dir = "web/js"
 fonts_dir = "web/fonts"
 
-output_style = :nested
+output_style = :compressed #:nested
 
 # To enable relative paths to assets via compass helper functions. Uncomment:
 # relative_assets = true
 
 # To disable debugging comments that display the original location of your selectors. Uncomment:
-# line_comments = false
+line_comments = false
 color_output = false
 
 
